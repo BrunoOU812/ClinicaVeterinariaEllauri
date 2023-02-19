d4822f34c70b4119f2798734eff2d14756bc6a36
Bruno Morgades <brunomorgades@hotmail.com>
2023-02-19 17:00:59 -0300

    Subhero

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,5 +1,35 @@
 import React from "react";
-
+import Card from "./Card";
+import Logo from "../images/Logo.svg";
+import { FaPlusCircle, FaBriefcaseMedical, FaBone } from "react-icons/fa";
+import { MdOutlinePets } from "react-icons/md";
 export default function Body() {
-  return <div>Body</div>;
+  return (
+    <div className="bg-blackish p-[20px] grid grid-cols-2 gap-[20px]  ">
+      <Card
+        titulo="Medicina"
+        logo={<FaPlusCircle />}
+        image={Logo}
+        text="asdfasdf sadfasdfsa dfsadfsadf sdfsadfsad sdf"
+      />
+      <Card
+        titulo="Medicina"
+        logo={<MdOutlinePets />}
+        image={Logo}
+        text="asdfasdf sadfasdfsa dfsadfsadf sdfsadfsad sdf"
+      />
+      <Card
+        titulo="Medicina"
+        logo={<FaBriefcaseMedical />}
+        image={Logo}
+        text="asdfasdf sadfasdfsa dfsadfsadf sdfsadfsad sdf"
+      />
+      <Card
+        titulo="Medicina"
+        logo={<FaBone />}
+        image={Logo}
+        text="asdfasdf sadfasdfsa dfsadfsadf sdfsadfsad sdf"
+      />
+    </div>
+  );
 }
