1fc0fa412be2c0fd922bce60de3ffb5d488c30ca
Bruno Morgades <brunomorgades@hotmail.com>
2023-02-28 00:48:28 -0300

    minor adjustments

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -24,7 +24,14 @@ export default function Location({ id, title, image, paragraph }) {
           <div className="space-y-[20px] flex flex-col sm:flex-row-reverse  ">
             <div className="w-full flex-1 flex sm:pl-[20px] ">
               <div className="flex-1 bg-whiteish rounded-xl  h-[250px] overflow-hidden flex item-center justify-center">
-                <img src={Demo} alt="" className=" object-cover w-full" />
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3270.946402283568!2d-56.1493833!3d-34.9071711!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x959f810e747dbfdd%3A0xeb0e827479fb8414!2sCl%C3%ADnica%20Veterinaria%20Ellauri!5e0!3m2!1sen!2suy!4v1646057767594!5m2!1sen!2suy"
+                  width="100%"
+                  height="100%"
+                  title="Map"
+                  allowfullscreen=""
+                  loading="lazy"
+                ></iframe>
               </div>
             </div>
             <div className="flex-1 sm:pr-[20px]">{paragraph}</div>
